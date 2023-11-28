b86c955479764cff7987072f26af5a437bed1795
Faluminus <108215571+Faluminus@users.noreply.github.com>
2023-11-28 21:36:20 +0200

    Can install now , bit broken

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 let element = document.getElementById("content_video_html5_api").src;
 if(element!=null){
+    let name = window.location.pathname + ".mp4";
     window.open(element);
+    if(window.location.host = 'storage4-2.premiumcdn.net'){
+        let link = document.createElement('a');
+        link.href = window.location.href;
+        link.setAttribute('download',name);
+    
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
 }
